be13abeb8e0e9a1bbb67b351db74fe1e08a07fda
Tofuman <d.worth@btinternet.com>
2021-11-24 21:39:53 +0000

    Corrected the way the MMDL file was being read. It should now be accurate.

--- readmodel.py
+++ readmodel.py
@@ -1,5 +1,6 @@
 import sys
 import struct
+import array
 
 modelfile = ''
 logfile = ''
@@ -31,116 +32,165 @@ def main(argv):
 
 def readmmdl():
     file = open(modelfile, "rb")
-    filetype = file.read(4)
-    if (filetype != b"LDMM"):
+    fileType = file.read(4)
+    if (fileType != b"LDMM"):
         raise Exception('Not An MMDL File.')
     logoutput(f"File: {modelfile}")
-    mattablesize = toInt(file.read(4))
-    logoutput(f"Material table size: {mattablesize}")
-    verttablesize = toInt(file.read(4))
-    logoutput(f"Vertices table size: {verttablesize}")
-    facetablesize = toInt(file.read(4))
-    logoutput(f"Face table size: {facetablesize}")
-    unknown1 = toInt(file.read(4))
-    logoutput(f"Unknown value 1: {hex(unknown1)}")
-    vertsize = toInt(file.read(4))
-    logoutput(f"Vertice size: {vertsize}")
-    vertcount = toInt(file.read(4))
-    logoutput(f"Vertice count: {vertcount}")
-    facesize = toInt(file.read(4))
-    logoutput(f"Face size: {facesize}")
-    facecount = toInt(file.read(4))
-    logoutput(f"Face count: {facecount}")
+    matTableSize = toInt(file.read(4))
+    logoutput(f"Material table size: {matTableSize}")
+    vertTableSize = toInt(file.read(4))
+    logoutput(f"Vertices table size: {vertTableSize}")
+    faceTableSize = toInt(file.read(4))
+    logoutput(f"Face table size: {faceTableSize}")
+    modelType = toInt(file.read(4))
+    logoutput(f"Unknown value 1: {hex(modelType)}")
+    vertSize = toInt(file.read(4))
+    logoutput(f"Vertice size: {vertSize}")
+    vertCount = toInt(file.read(4))
+    logoutput(f"Vertice count: {vertCount}")
+    faceSize = toInt(file.read(4))
+    logoutput(f"Face size: {faceSize}")
+    faceCount = toInt(file.read(4))
+    logoutput(f"Face count: {faceCount}")
     # Material Table
-    # TODO: Process material table header
-    file.seek(80)
-    matcount = 0
+    matCount = 0
     logbuf = ''
-    while (file.tell() < (16 + mattablesize)):
-        matcount += 1
-        curroffset = file.tell()
-        matid = toInt(file.read(4))
-        matflag = toInt(file.read(4))
-        matopacity = toFloat(file.read(4))
-        logbuf += f"Current Offset: {curroffset}\n"
-        logbuf += f"\nCurrent Material: {matcount}\n"
-        logbuf += f"Mat ID: {matid}\n"
-        logbuf += f"Mat Flag: {hex(matflag)}\n"
-        if(matflag == 0):
-            # Is Mat type
-            matname = toStr(file.read(32))
-            unknownmat1 = toInt(file.read(4))
-            matname2 = toStr(file.read(32))
-            logbuf += f"Mat Name: {matname}\n"
-            logbuf += f"Mat Name 2: {matname2}\n"
-            logbuf += f"{toInt(file.read(4))}, {toInt(file.read(4))}\n"
-            curroffset = file.tell()
-            if (toInt(file.read(2)) == 12336):
-                # If has '00' then is string and structure different
-                file.seek(curroffset)
-                matname3 = toStr(file.read(32))
-                logbuf += f"Mat Name 3: {matname3}\n"
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toInt(file.read(4))}, {toInt(file.read(4))}, {toInt(file.read(4))}, {toInt(file.read(4))}, "
-                logbuf += f"{toInt(file.read(4))}, {toInt(file.read(4))}, {toInt(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}\n"
-            else:
-                file.seek(curroffset)
-                logbuf += f"{toInt(file.read(4))}, {toInt(file.read(4))}, {toInt(file.read(4))}, {toInt(file.read(4))}\n"
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}\n"
-        else:
-            # Is regular type
-            matname = toStr(file.read(24))
-            if(matname == ''):
-                file.seek(curroffset + 18)
-                unknownvalue = toInt(file.read(2))
-                file.seek(curroffset + 16)
-                logbuf += "Mat Name: [NO NAME]\n"
-                if (unknownvalue != 0):
-                    logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}\n"
+
+    while (file.tell() < (16 + matTableSize)):
+        matCount += 1
+        currOffset = file.tell()
+        matOpacity = 0.0
+        if(matCount > 1): # First Entry doesn't have Opacity
+            matOpacity = toFloat(file.read(4))
+        if ((matCount == 1) or not (toInt(file.read(4)) & 0xFF000000)): # No Name
+            matName = "[NO NAME]"
+            logbuf += f"\nCurrent Offset: {currOffset}\n"
+            logbuf += f"Current Material: {matCount}\n"
+            logbuf += f"Mat Name 1: {matName}\n"
+            if (matCount == 1):
+                currOffset += 4
+                file.seek(currOffset)
+            matUnknown1 = toFloat(file.read(4))
+            if (toInt(bytearray(struct.pack('f', matUnknown1))) & 0xFFFF0000): # lame check for if float value
+                matUnknown2 = toFloat(file.read(4))
+                matUnknown3 = toFloat(file.read(4))
+                matUnknown4 = toFloat(file.read(4))
+                matUnknown5 = toFloat(file.read(4))
+                matUnknown6 = toFloat(file.read(4))
+                if(matCount == 1): # First Entry has more data
+                    matUnknown7 = toInt(file.read(4))
+                    matUnknown8 = toInt(file.read(4))
+                    matUnknown9 = toInt(file.read(4))
+                    matUnknown10 = toInt(file.read(4))
+
+                matType = toInt(file.read(4))
+                matId = toInt(file.read(4))
+                logbuf += f"Mat Type: {matType}\n"
+                logbuf += f"Mat ID: {hex(matId)}\n"
+                if(matCount > 1): # First Entry doesn't have Opacity            
+                    logbuf += f"Mat Opacity: {matOpacity}\n" 
+
+                if(matCount == 1): # First Entry has more data   
+                    logbuf += f"{matUnknown1}, {matUnknown2}, {matUnknown3}, {matUnknown4}, "
+                    logbuf += f"{matUnknown5}, {matUnknown6}, {matUnknown7}, {matUnknown8}, "
+                    logbuf += f"{matUnknown9}, {matUnknown10}\n"
                 else:
-                    # Is entry before mat type
-                    logbuf += f"{toInt(file.read(4))}, {toInt(file.read(4))}, {toInt(file.read(4))}, "
-                    logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                    logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                    logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                    logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}\n"
-            else:
-                logbuf += f"Mat Name: {matname}\n"
-                logbuf += f"{toInt(file.read(4))}\n"
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}, "
-                logbuf += f"{toFloat(file.read(4))}\n"
-                logbuf += f"{toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}\n"
-    logbuf += f"-------------------------\nTotal Materials: {matcount}\n-------------------------\n"
+                    logbuf += f"{matUnknown1}, {matUnknown2}, {matUnknown3}, {matUnknown4}, "
+                    logbuf += f"{matUnknown5}, {matUnknown6}\n"
+                
+            else: # Is material assigned to object
+                file.seek(currOffset + 4)
+                matUnknown1 = toInt(file.read(4))
+                matUnknown2 = toInt(file.read(4))
+                matUnknown3 = toInt(file.read(4))
+                matUnknown4 = toInt(file.read(4))
+                matUnknown5 = toFloat(file.read(4))
+                matUnknown6 = toFloat(file.read(4))
+                matUnknown7 = toFloat(file.read(4))
+                matUnknown8 = toFloat(file.read(4))
+                matUnknown9 = toFloat(file.read(4))
+                matUnknown10 = toFloat(file.read(4))
+                matUnknown11 = toFloat(file.read(4))
+                matUnknown12 = toFloat(file.read(4))
+                matUnknown13 = toFloat(file.read(4))
+                matUnknown14 = toFloat(file.read(4))
+                matUnknown15 = toFloat(file.read(4))
+                matUnknown16 = toFloat(file.read(4))
+                matUnknown17 = toFloat(file.read(4))
+                matUnknown18 = toFloat(file.read(4))
+                matUnknown19 = toFloat(file.read(4))
+                matUnknown20 = toFloat(file.read(4))
+                matUnknown21 = toFloat(file.read(4))
+                matName2 = toStr(file.read(32))
+                matUnknown22 = toFloat(file.read(4))
+                matName3 = toStr(file.read(32))
+                matId = toInt(file.read(4))
+                logbuf += f"Mat Name 2: {matName2}\n"
+                logbuf += f"Mat Name 3: {matName3}\n"
+                logbuf += f"Mat ID: {hex(matId)}\n"  
+                logbuf += f"{matUnknown1}, {matUnknown2}, {matUnknown3}, {matUnknown4}, "
+                logbuf += f"{matUnknown5}, {matUnknown6}, {matUnknown7}, {matUnknown8}, "
+                logbuf += f"{matUnknown9}, {matUnknown10}, {matUnknown11}, {matUnknown12}, "
+                logbuf += f"{matUnknown13}, {matUnknown14}, {matUnknown15}, {matUnknown16}, "
+                logbuf += f"{matUnknown17}, {matUnknown18}, {matUnknown19}, {matUnknown20}, "
+                logbuf += f"{matUnknown21}\n"      
+        else: # Has Name
+            file.seek(currOffset + 4)
+            matName = toStr(file.read(32))
+            logbuf += f"\nCurrent Offset: {currOffset}\n"
+            logbuf += f"Current Material: {matCount}\n"
+            logbuf += f"Mat Name 1: {matName}\n"
+            matUnknown1 = toFloat(file.read(4))
+            matUnknown2 = toFloat(file.read(4))
+            matUnknown3 = toFloat(file.read(4))
+            matUnknown4 = toFloat(file.read(4))
+            matUnknown5 = toFloat(file.read(4))
+            matUnknown6 = toFloat(file.read(4))
+            matUnknown7 = toFloat(file.read(4))
+            matUnknown8 = toFloat(file.read(4))
+            matUnknown9 = toFloat(file.read(4))
+            matUnknown10 = toFloat(file.read(4))
+            matUnknown11 = toFloat(file.read(4))
+            matUnknown12 = toFloat(file.read(4))
+            matUnknown13 = toFloat(file.read(4))
+            matUnknown14 = toFloat(file.read(4))
+            matUnknown15 = toFloat(file.read(4))
+            matUnknown16 = toFloat(file.read(4))
+            matUnknown17 = toFloat(file.read(4))
+            matUnknown18 = toFloat(file.read(4))
+            matUnknown19 = toFloat(file.read(4))
+            matUnknown20 = toFloat(file.read(4))
+            matUnknown21 = toFloat(file.read(4))
+            matUnknown22 = toFloat(file.read(4))
+            matUnknown23 = toFloat(file.read(4))
+            matType = toInt(file.read(4))
+            matId = toInt(file.read(4))
+            logbuf += f"Mat Type: {matType}\n"
+            logbuf += f"Mat ID: {hex(matId)}\n"
+            if(matCount > 1): # First Entry doesn't have Opacity            
+                logbuf += f"Mat Opacity: {matOpacity}\n" 
+            logbuf += f"{matUnknown4}, {matUnknown5}, {matUnknown6}, {matUnknown7}, "
+            logbuf += f"{matUnknown8}, {matUnknown9}, {matUnknown10}, {matUnknown11}, "
+            logbuf += f"{matUnknown12}, {matUnknown13}, {matUnknown14}, {matUnknown15}, "
+            logbuf += f"{matUnknown16}, {matUnknown17}, {matUnknown18}, {matUnknown19}, "
+            logbuf += f"{matUnknown20}, {matUnknown21}, {matUnknown22}, {matUnknown23}\n"
+    logbuf += f"\n-------------------------\nTotal Materials: {matCount}\n-------------------------\n"
     logoutput(logbuf)
+    
     # Vertice Table
-    file.seek(16 + mattablesize)
+    file.seek(16 + matTableSize)
     logbuf = ''
-    for i in range(vertcount):
-        if (vertsize == 36):
-            logbuf += f"Vertice {i + 1}: {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {hex(toInt(file.read(4)))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}\n"
-        elif (vertsize == 24):
+    for i in range(vertCount):
+        if (vertSize == 36):
+            logbuf += f"Vertice {i + 1}: {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {hex(toInt(file.read(4)))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}\n"
+        elif (vertSize == 24):
             logbuf += f"Vertice {i + 1}: {toFloat(file.read(4))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}, {hex(toInt(file.read(4)))}, {toFloat(file.read(4))}, {toFloat(file.read(4))}\n"
     logoutput(logbuf)
+    
     # Face Table 
-    file.seek(16 + mattablesize + verttablesize)
+    file.seek(16 + matTableSize + vertTableSize)
     logbuf = ''
-    for i in range(int(facecount / 3)):
+    for i in range(int(faceCount / 3)):
         logbuf += f"Face {i + 1}: {toInt(file.read(2))}, {toInt(file.read(2))}, {toInt(file.read(2))}\n"
     logoutput(logbuf)
     file.close
